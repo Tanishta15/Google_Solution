8e31e0609e851442b7d07d285991672d4a254f87
pradyumna4 <114288088+pradyumna4@users.noreply.github.com>
2025-04-04 01:48:06 +0530

    check
    #edited to firebasestorage.app

--- GSC/firebase_config.py
+++ GSC/firebase_config.py
@@ -2,10 +2,10 @@ import firebase_admin
 from firebase_admin import credentials, firestore, storage
 
 # Load Firebase credentials
-cred = credentials.Certificate("/Users/tanishta/Desktop/GitHub/solution-4f24a-firebase-adminsdk-fbsvc-8d8a282892.json")  # Use the downloaded key
+cred = credentials.Certificate(r"C:\Users\Pradyu\Desktop\Projects\GSC\solution-4f24a-firebase-adminsdk-fbsvc-751ca90032.json")  # Use the downloaded key
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'solution-4f24a.firebasestorage.app'
-})
+}) #edited to firebasestorage.app
 # Firestore Database
 db = firestore.client()
 
--- GSC/kproducer.py
+++ GSC/kproducer.py
@@ -3,8 +3,8 @@ import json
 import firebase_admin
 from firebase_admin import credentials, storage
 
-cred = credentials.Certificate("/Users/tanishta/Desktop/GitHub/solution-4f24a-firebase-adminsdk-fbsvc-8d8a282892.json")  
-firebase_admin.initialize_app(cred, {"storageBucket": "solution-4f24a.appspot.com"})
+cred = credentials.Certificate(r"C:\Users\Pradyu\Desktop\Projects\GSC\solution-4f24a-firebase-adminsdk-fbsvc-751ca90032.json")  #downloaded key
+firebase_admin.initialize_app(cred, {"storageBucket": "solution-4f24a.firebasestorage.app"}) #edited to firebasestorage.app
 
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
